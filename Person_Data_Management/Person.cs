bf1db26dc4c64373aec0f729359c3527f448f88a
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-12-09 23:11:59 +0530

    Persons Data Management

---
+++ Person_Data_Management/Person.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Person_Data_Management
+{
+    internal class Person
+    {
+        public string SSN;
+        public string Name;
+        public string Address;
+        public int Age;
+
+        public Person(string ssn, string name, string address, int age)
+        {
+            SSN = ssn;
+            Name = name;
+            Address = address;
+            Age = age;
+        }
+    }
+}
---
+++ Person_Data_Management/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Person_Data_Management
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> listPersonsInCity = new List<Person>();
+            PersonRepository.AddRecords(listPersonsInCity);
+        }
+    }
+}
